f176e3b06b50a113d4b72e6e75673eca27349ff0
RogelioSales <salesrogelio.sales83@gmail.com>
2018-03-12 06:43:44 -0500

    Level selection

---
+++ Assets/Scripts/Selection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Selection : MonoBehaviour
+{
+    private GameObject[] tribeSelection;
+    private int selectIndex = 0;
+
+    private void Start()
+    {
+        tribeSelection = new GameObject[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            tribeSelection[i] = transform.GetChild(i).gameObject;
+        }
+        foreach(GameObject go in tribeSelection)
+        {
+            go.SetActive(false);
+        }
+        tribeSelection[selectIndex].SetActive(true);
+    }
+    public void Select(int index)
+    {
+        if (index == selectIndex)
+        {
+            return;
+        }
+        if (index < 0 || index >= tribeSelection.Length)
+        {
+            return;
+        }
+        tribeSelection[selectIndex].SetActive(false);
+        selectIndex = index;
+        tribeSelection[selectIndex].SetActive(true);
+      
+    }
+    public void PlayButton(int sceneIndex)
+    {
+        PlayerPrefs.SetInt("CharacterSelected", selectIndex);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
